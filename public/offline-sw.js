e7daa8d2d70c5148256ca35bb57bb9d491cc9117
Ctrl-Shift-Alvin <alvin.szoke@gmail.com>
2025-08-18 03:30:11 +0200

    fix: improve offline-sw.js and /offline/ page to handle server outage

--- public/offline-sw.js
+++ public/offline-sw.js
@@ -1,20 +1,28 @@
 /* eslint-disable import/unambiguous */
+
 const CACHE_VERSION = 'v1';
 const CACHE_NAME = `offline-cache-${CACHE_VERSION}`;
 const OFFLINE_URL = '/offline/';
 
+const PRECACHE_ASSETS = [
+	OFFLINE_URL,
+	'/favicon.ico'
+];
+
+// On Install: Pre-cache the offline page and assets
 self.addEventListener(
 	'install',
 	(event) => {
 
 		event.waitUntil(caches
 			.open(CACHE_NAME)
-			.then((cache) => cache.addAll([ OFFLINE_URL ])));
+			.then((cache) => cache.addAll(PRECACHE_ASSETS)));
 		self.skipWaiting();
 
 	}
 );
 
+// On Activation: Clean up old caches
 self.addEventListener(
 	'activate',
 	(event) => {
@@ -35,16 +43,17 @@ self.addEventListener(
 	}
 );
 
+// On Fetch: Intercept navigation requests to provide an offline fallback
 self.addEventListener(
 	'fetch',
 	(event) => {
 
-		if (event.request.method !== 'GET')
-			return;
+		// Only handle navigation requests
+		if (event.request.mode === 'navigate') {
 
-		event.respondWith(fetch(event.request).catch(() => caches
-			.match(event.request)
-			.then((response) => response || caches.match(OFFLINE_URL))));
+			event.respondWith(fetch(event.request).catch(() => caches.match(OFFLINE_URL)));
+
+		}
 
 	}
 );
