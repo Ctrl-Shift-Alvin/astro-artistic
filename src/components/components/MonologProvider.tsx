fabffa117d88c2f32bd45983f94d1e919e05213a
Ctrl-Shift-Alvin <alvin.szoke@gmail.com>
2025-09-09 23:06:01 +0200

    feat: add Monolog.showAsync

--- src/components/components/MonologProvider.tsx
+++ src/components/components/MonologProvider.tsx
@@ -4,6 +4,14 @@ import {
 	MonologPopup, MonologPosition
 } from './MonologPopup';
 
+export type TMonologProps = {
+	text: string;
+	durationMs?: number;
+	dialogPosition?: MonologPosition;
+	fadeDurationMs?: number;
+	className?: string;
+};
+
 // eslint-disable-next-line @typescript-eslint/no-extraneous-class
 export class Monolog {
 
@@ -13,13 +21,7 @@ export class Monolog {
 		dialogPosition = MonologPosition.Bottom,
 		fadeDurationMs = 750,
 		className
-	}: {
-		text: string;
-		durationMs?: number;
-		dialogPosition?: MonologPosition;
-		fadeDurationMs?: number;
-		className?: string;
-	}) {
+	}: TMonologProps) {
 
 		const monologContainer = document.createElement('div');
 		document.documentElement.appendChild(monologContainer);
@@ -49,6 +51,51 @@ export class Monolog {
 			),
 			durationMs + fadeDurationMs
 		);
+
+	}
+
+	static showAsync({
+		text,
+		durationMs = 2000,
+		dialogPosition = MonologPosition.Bottom,
+		fadeDurationMs = 750,
+		className
+	}: TMonologProps) {
+
+		const monologContainer = document.createElement('div');
+		document.documentElement.appendChild(monologContainer);
+		const root = createRoot(monologContainer);
+
+		const cleanup = () => {
+
+			root.unmount();
+			document.documentElement.removeChild(monologContainer);
+
+		};
+
+		root.render((
+			<MonologPopup
+				text={text}
+				dialogPosition={dialogPosition}
+				durationMs={durationMs}
+				fadeDurationMs={fadeDurationMs}
+				className={className}
+			/>
+		));
+
+		return new Promise<void>((res) => {
+
+			setTimeout(
+				() => {
+
+					cleanup();
+					res();
+
+				},
+				durationMs + fadeDurationMs
+			);
+
+		});
 
 	}
 
