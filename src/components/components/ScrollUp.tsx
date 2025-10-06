55a8d348ec6881017e3cd1f139e6350c6fbd6085
Ctrl-Shift-Alvin <alvin.szoke@gmail.com>
2025-10-06 15:28:28 +0200

    feat: make 'ScrollUp' sticky at the bottom to make sure it doesn't cover content on smaller screens

--- src/components/components/ScrollUp.tsx
+++ src/components/components/ScrollUp.tsx
@@ -55,34 +55,38 @@ export const ScrollUp = ({ scrollYThreshold }: { scrollYThreshold: number }) => 
 		<div
 			className={
 				clsx(
-					'fixed right-10 bottom-10 z-50 rounded-full border-2 border-white p-2 transition-opacity',
+					'sticky bottom-5 right-5 w-full flex flex-row-reverse z-50 p-2 transition-opacity',
 					visible
 						? 'pointer-events-auto cursor-pointer opacity-100'
 						:	'pointer-events-none opacity-0'
 				)
 			}
-			onClick={
-				() => {
-
-					smoothScroll(0);
-
-				}
-			}
 		>
-			<svg
-				className={'size-6 text-white'}
-				xmlns={'http://www.w3.org/2000/svg'}
-				fill={'none'}
-				viewBox={'0 0 24 24'}
-				stroke={'currentColor'}
-				strokeWidth={'2'}
+			<div
+				className={'flex size-12 items-center justify-center rounded-full border-2 border-white'}
+				onClick={
+					() => {
+
+						smoothScroll(0);
+
+					}
+				}
 			>
-				<path
-					strokeLinecap={'round'}
-					strokeLinejoin={'round'}
-					d={'M5 15l7-7 7 7'}
-				/>
-			</svg>
+				<svg
+					className={'size-6 text-white'}
+					xmlns={'http://www.w3.org/2000/svg'}
+					fill={'none'}
+					viewBox={'0 0 24 24'}
+					stroke={'currentColor'}
+					strokeWidth={'2'}
+				>
+					<path
+						strokeLinecap={'round'}
+						strokeLinejoin={'round'}
+						d={'M5 15l7-7 7 7'}
+					/>
+				</svg>
+			</div>
 		</div>
 	);
 
