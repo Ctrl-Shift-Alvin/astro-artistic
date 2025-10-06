3dae9cbb6e025363e2e88941a42e53c50dc6b627
Ctrl-Shift-Alvin <alvin.szoke@gmail.com>
2025-10-06 23:48:52 +0200

    fix: misleading 'minDays' config value in 'shared/config/events.ts'

--- src/shared/config/events.ts
+++ src/shared/config/events.ts
@@ -3,7 +3,7 @@ import { ZEventsConfig } from './configTypes';
 export const EventsConfig = ZEventsConfig.parse({
 	showLinks: true,
 	ageRangeShown: {
-		minDays: 14,
+		minDays: -14,
 		maxDays: Number.MAX_SAFE_INTEGER
 	}
 });
